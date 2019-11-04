f35c31166e6e60d2ece6a9b5e09a5c91c94ede1b
Matheus <matheusbernardi@santins.com.br>
2019-11-04 20:35:35 -0300

    Novo tema

--- wp-content/themes/pedras-vidros/front-page.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-
-<main class="main-container">
-
-	<componente-vue msg="Este é um componente Vue"></componente-vue>
-
-</main>
-
-<?php get_footer(); ?>
--- wp-content/themes/pedras-vidros/index.php
+++
@@ -1,7 +0,0 @@
-<?php get_header(); ?>
-
-	<div class="container">
-		<h1><?php _e( 'Página do Blog.' , ' nnn' ) ?></h1>
-	</div>
-
-<?php get_footer(); ?>
